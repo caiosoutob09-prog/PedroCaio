245298b6be4b9097af5ced6133b57dfafbf55c0c
caiosoutob09-prog <caiosoutob09@gmail.com>
2026-03-21 22:23:35 -0300

    carrouseulup2

--- Metplay/teste.js
+++ Metplay/teste.js
@@ -8,21 +8,22 @@ carousel.addEventListener('slid.bs.carousel', function (event) {
     if (i == 0) {
         document.getElementById("cardTitle").innerText = "Outono Chegou!";
         document.getElementById("cardText").innerText = "Descontos de até 70% em jogos incríveis. Corre que é por tempo limitado!";
-
+        document.getElementById("cardImg").src = "Ofert1.png";
     }
     if (i == 1) {
         document.getElementById("cardTitle").innerText = "Forza Horizon 5";
         document.getElementById("cardText").innerText = "Explore as paisagens vibrantes de mundo aberto do México com diversão e velocidade sem limites com os melhores carros do mundo.";
+        document.getElementById("cardImg").src = "capa1.jpg";
     }
     if (i == 2) {
         document.getElementById("cardTitle").innerText = "Resident Evil: Requiem";
         document.getElementById("cardText").innerText = "Réquiem para os mortos. Pesadelo para os vivos. Prepare-se para escapar da morte em uma experiência arrepiante e de tirar o fôlego.";
-
+        document.getElementById("cardImg").src = "capa2.jpg";
     }
     if (i == 3) {
         document.getElementById("cardTitle").innerText = "The Elder Scrolls V: Skyrim";
-        document.getElementById("cardText").innerText = "Vencedor de mais de 200 prêmios de Jogo do Ano, The Elder Scrolls V: Skyrim Special Edition traz vida à fantasia épica com detalhes deslumbrantes. A Special Edition inclui o jogo aclamado pela crítica e complementos com novos recursos.";
-
+        document.getElementById("cardText").innerText = "Vencedor de mais de 200 prêmios de Jogo do Ano, The Elder Scrolls V: Skyrim Special Edition traz vida à fantasia épica com detalhes deslumbrantes.";
+        document.getElementById("cardImg").src = "capa3.jpg";
     }
 
 });
