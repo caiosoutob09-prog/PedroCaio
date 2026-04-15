7a713dd66251bf831d87b74fd86b8e65775f1f00
caiosoutob09-prog <caiosoutob09@gmail.com>
2026-04-15 15:45:59 -0300

    15/04

--- Metplay/alerta.js
+++ Metplay/alerta.js
@@ -1,21 +1,13 @@
 $(document).ready(function () {
 
-    $(btndesejo).click(function () { 
+    $("#btndesejo").click(function () { 
         
         Swal.fire({
-        title: "Adicionado a lista de desejo!",
-        icon: "success",
-        draggable: true
-
-        
-    });
-
-    
-
+            title: "Adicionado à lista de desejos!",
+            icon: "success",
+            draggable: true
+        });
 
     });
-    
-
-    
 
 });
