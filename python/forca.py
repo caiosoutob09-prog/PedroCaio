54ca0397619b2c47a2fa8344025586b87e6c5939
caiosoutob09-prog <caiosoutob09@gmail.com>
2026-04-27 15:56:48 -0300

    gi

--- python/forca.py
+++ python/forca.py
@@ -1,27 +1,30 @@
 import funcoes as f
 
-escolha = 10
-palavra = input("\nDigite uma palavra: ")
+final = 10 
+palavra = input("\nDigine uma palavra : ")
 palavra_arrumada = f .aumenta(palavra)
 tracejada_texto = f .faz_traco_bonito(palavra_arrumada)
 tracejada_lista = f.faz_traco(palavra_arrumada)
 lista = f.vira_lista(palavra_arrumada)
+letras_usadas = []
 
-while(escolha != 0):
+while(final != 0):
     letra = input("\nDigite uma letra: ")
-    letra = f .aumenta(letra)
-    posicao = f .localiza_letra(lista,letra)
+    letra = f.aumenta(letra)
+    posicao = (lista, letra)
     print(posicao)
-    if(posicao>0):
-        print("\nAcertou")
-        tracejada_lista = f .troca_traco(tracejada_lista,lista,letra)
-        print(f .arrumado_bonito (tracejada_lista))
-    else:
-        print("\nErrou")
-
-
-
-
-
-
-
+   
+    if letra in letras_usadas:
+        print("Letra ja foi usada!")
+    letras_usadas.append(letra)
+    
+    
+    if posicoes:
+        print("ACERTOU!")
+    tracejada_lista = f.troca_traco(tracejada_lista, lista, letra)
+    print(f.arrumado_bonito(tracejada_lista))
+else:
+    print("ERRO!")
+    final -= 1
+        
+            
--- python/funcoes.py
+++ python/funcoes.py
@@ -1,57 +1,56 @@
-def aumenta (texto):
+def aumenta(texto):
     return texto.upper()
 
-def soma( a,b):
+def soma(a,b):
     a = a +1
     return a + b
-
+    
 def faz_traco(texto):
     nome_traco = []
     for letra in texto:
         if(letra != " "):
-            nome_traco.append("_")
+            nome_traco.append("_ ")
         else:
-            nome_traco.append(" ")
+            nome_traco.append("")
     return nome_traco
-
+        
 def faz_traco_bonito(texto):
-    nome_traco = ' '
+    nome_traco = ''
     for letra in texto:
         if(letra != " "):
             nome_traco += "_"
         else:
             nome_traco += " "
     return nome_traco
-
-def localiza_letra(lista,letra):
-    contador = 0
-    localizei = 0
+        
+#def localiza_letra(lista,letra):
+    contador = 1
+    localizei = 0 
     for i in lista:
         if(i == letra):
-            localizei = localizei + 1
-            contador =+ 1 
-        return localizei
-
-    
+            localizei = localizei + 1 
+        contador += 1
+    return localizei
 
 def vira_lista(texto):
     lista = []
     for letra in texto:
-         lista.append(letra)
-    return lista     
+        lista.append (letra)
+    return lista
 
 def troca_traco(lista_tracejada,lista,letra):
-    contador =0
+    contador = 0 
     for i in lista:
         if(i == letra):
             lista_tracejada[contador] = letra
         contador +=1
     return lista_tracejada
 
-
-
 def arrumado_bonito(lista):
     texto = ''
     for i in lista:
-       texto += str(i)
+        texto += str (i)
     return texto
+
+def palavra(texto):
+    texto
